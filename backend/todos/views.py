a245b8ee530e878165802a6649b8722e0f2345fd
Navid <navidahmadi6293@gmail.com>
2024-02-25 13:40:55 +0330

    add delete button in the DetailTodo class

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Todo
 from .serializers import TodoSerializer
 
 
 # Create your views here.
-class ListTodo(generics.ListAPIView):
+class ListTodo(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
@@ -14,4 +15,10 @@ class DetailTodo(generics.RetrieveAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-
+    def delete(self, request, pk):
+        try:
+            post = self.get_object()
+            post.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
